df929986829bdd6728403492d42b7b0cf6ced358
Qruzz <quamruzzaman@Qruzzs-MacBook-Air.local>
2022-01-24 13:30:21 +0530

    first commit

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -23,13 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')l_2jf&aqqywl^_=$er7vpfz2+4!&lk(74d$^of2twha9$bqvn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'https://e-commerce-django-qruzz.herokuapp.com/'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -129,6 +125,7 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_my_proj"),
